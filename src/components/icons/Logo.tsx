0f382a2a2eca05146930681497990e3041accb43
Nnamdi <17215830+iamendy@users.noreply.github.com>
2024-02-17 13:33:23 +0100

    chore: removed unnecessary tags

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,17 +1,8 @@
 const Logo = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      className=" w-10 h-10"
-    >
+    <svg className=" w-10 h-10">
       <use href="#svg4125748904">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 39 26"
-          id="svg4125748904"
-        >
+        <svg viewBox="0 0 39 26" id="svg4125748904">
           <path
             d="M 26.011 0.52 L 21.789 4.778 L 28.075 10.887 L 30.045 12.924 L 28.075 14.867 L 21.789 21.069 L 26.011 25.235 L 38.582 12.924 Z"
             fill="rgba(255, 255, 255, 0.5)"
